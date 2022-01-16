04449347321f39800ada5c89b7daf3cb263a2230
ankit <devaniankit1990@gmail.com>
2022-01-16 15:29:17 +0530

    Array 7 done

---
+++ Array/4.py
@@ -0,0 +1,3 @@
+def sort012(self,arr,n):
+
+       return arr.sort()
---
+++ Array/6.py
@@ -0,0 +1,18 @@
+class Solution:    
+
+    def doUnion(self,a,n,b,m):
+        a.extend(b)
+        a=set(a)
+        return len(a)
+
+if __name__=='__main__':
+    t=int(input())
+    for _ in range(t):
+        
+        n,m=[int(x) for x in input().strip().split()]
+        
+        a=[int(x) for x in input().strip().split()]
+        b=[int(x) for x in input().strip().split()]
+        ob=Solution()
+        
+        print(ob.doUnion(a,n,b,m))
